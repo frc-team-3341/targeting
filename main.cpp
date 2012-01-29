d6954a66fb4dc40b2d088fefdee6a1ce51a630e7
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2012-01-28 19:17:40 -0800

    Restructured Matrices
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char* argv[]) {
     // Get Video Capture Device
     cap.open(atoi(argv[1]));
     if (!cap.isOpened()) {
-      cerr <<"Cannot open capture device." <<endl;
+      cerr <<"Unable to open capture device " <<argv[1] <<"." <<endl;
       return -1;
     }
   }
@@ -58,79 +58,61 @@ int main(int argc, char* argv[]) {
   namedWindow("Corners Green Channel", 0);
   //namedWindow("Canny Red Channel", 0);
   
-  for (int frameCount=1;; frameCount++) {
+  for (int frameCount=1;; ++frameCount) {
     Mat img;
     if (isFile)
       img=imread(argv[1]); // Load Image from File
     else
       cap >>img; // Load Image from Video Capture Device
     //cvtColor(img, img, CV_RGB2HSV); // Convert Image to HSV
-    Mat img_gray;
-    cvtColor(img, img_gray, CV_RGB2GRAY); // Convert Image to Grayscale
-    Mat img_gray_thresh;
-    Mat img_gray_canny;
-    Mat img_gray_corners;
-    vector<Point2f> img_gray_cornerPoints;
-    vector<Mat> img_rgb_planes;
-    vector<Mat> img_rgb_planes_thresh;
-    vector<Mat> img_rgb_planes_canny;
-    vector<Mat> img_rgb_planes_corners;
-    vector< vector<Point2f> > img_rgb_planes_cornerPoints;
+    vector<Mat> img_planes; // Order: Blue, Green, Red, Gray
+    vector<Mat> img_planes_thresh;
+    vector<Mat> img_planes_canny;
+    vector<Mat> img_planes_corners;
+    vector< vector<Point2f> > img_planes_cornerPoints;
     
-    split(img, img_rgb_planes); // Split Image into RGB
-    img_rgb_planes_thresh.resize(img_rgb_planes.size()); // Allocate Memory to Threshold Output Vector
-    img_rgb_planes_canny.resize(img_rgb_planes.size()); // Allocate Memory to Canny Output Vector
-    img_rgb_planes_cornerPoints.resize(img_rgb_planes.size()); // Allocate Memory to Corner Detection Output Vector
+    split(img, img_planes); // Split Image into RGB
+    img_planes.resize(img_planes.size() + 1); // Allocate Memory
+    cvtColor(img, img_planes.at(3), CV_RGB2GRAY); // Convert Image to Grayscale
+
+    // Allocate Memory to Vectors
+    img_planes_thresh.resize(img_planes.size());
+    img_planes_canny.resize(img_planes.size());
+    img_planes_cornerPoints.resize(img_planes.size());
 
     // Run Thresholding
     int thresholdType=CV_THRESH_BINARY;
-    threshold(img_gray, img_gray_thresh, lowerthresh, upperthresh, thresholdType);
-    threshold(img_rgb_planes.at(0), img_rgb_planes_thresh.at(0), lowerthresh, upperthresh, thresholdType);
-    threshold(img_rgb_planes.at(1), img_rgb_planes_thresh.at(1), lowerthresh, upperthresh, thresholdType);
-    threshold(img_rgb_planes.at(2), img_rgb_planes_thresh.at(2), lowerthresh, upperthresh, thresholdType);
+    for (unsigned i=0; i < img_planes.size(); ++i)
+      threshold(img_planes.at(i), img_planes_thresh.at(i), lowerthresh, upperthresh, thresholdType);
     
-
     // Run Canny
     int apertureSize=7;
-    Canny(img_gray_thresh, img_gray_canny, lowerthresh, upperthresh, apertureSize);
-    Canny(img_rgb_planes_thresh.at(0), img_rgb_planes_canny.at(0), lowerthresh, upperthresh, apertureSize);
-    Canny(img_rgb_planes_thresh.at(1), img_rgb_planes_canny.at(1), lowerthresh, upperthresh, apertureSize);
-    Canny(img_rgb_planes_thresh.at(2), img_rgb_planes_canny.at(2), lowerthresh, upperthresh, apertureSize);
-    //dilate(img_gray_canny, img_gray_canny, Mat(), Point(-1,-1)); 
+    for (unsigned i=0; i < img_planes_thresh.size(); ++i)
+      Canny(img_planes_thresh.at(i), img_planes_canny.at(i), lowerthresh, upperthresh, apertureSize);
 
     // Run Corner Detection
     // Reference: http://www.moosechips.com/2008/08/opencv-corner-detection-using-cvgoodfeaturestotrack/
     int maxCorners=10;
     double qualityLevel=0.1;
     int minDistance=10;
-    goodFeaturesToTrack(img_gray_thresh, img_gray_cornerPoints, maxCorners, qualityLevel, minDistance);
-    goodFeaturesToTrack(img_rgb_planes_thresh.at(0), img_rgb_planes_cornerPoints.at(0), maxCorners, qualityLevel, minDistance);
-    goodFeaturesToTrack(img_rgb_planes_thresh.at(1), img_rgb_planes_cornerPoints.at(1), maxCorners, qualityLevel, minDistance);
-    goodFeaturesToTrack(img_rgb_planes_thresh.at(2), img_rgb_planes_cornerPoints.at(2), maxCorners, qualityLevel, minDistance); 
-
-    // Print Coordinates of Corners
-    /*for (unsigned i=0; i < img_rgb_planes_cornerPoints.at(1).size(); i++) {
-      cout <<"0: " <<i <<": " <<img_rgb_planes_cornerPoints.at(1).at(i) <<endl;
-    }
-    for (unsigned i=0; i < img_rgb_planes_cornerPoints.size(); i++) {
-      for (unsigned j=0; j < img_rgb_planes_cornerPoints.at(i).size(); j++)
-	cout <<i+1 <<": " <<j <<": " <<img_rgb_planes_cornerPoints.at(i).at(j) <<endl;
-	}*/
+    for (unsigned i=0; i < img_planes_thresh.size(); ++i)
+      goodFeaturesToTrack(img_planes_thresh.at(i), img_planes_cornerPoints.at(i), maxCorners, qualityLevel, minDistance);
 
     // Create Corner Detection Output Images
-    img_gray_corners=img_gray_canny.clone();
-    for (unsigned i=0; i < img_rgb_planes_canny.size(); i++)
-      img_rgb_planes_corners.push_back(img_rgb_planes_canny.at(i).clone());
+    for (unsigned i=0; i < img_planes_canny.size(); ++i)
+      img_planes_corners.push_back(img_planes_canny.at(i).clone());
 
     // Write Corners to Output Image
-    for (unsigned i=0; i < img_rgb_planes_cornerPoints.at(1).size(); i++) {
-      int radius=img.rows/25;
-      int pointX=img_rgb_planes_cornerPoints.at(1).at(i).x;
-      int pointY=img_rgb_planes_cornerPoints.at(1).at(i).y;
-      Point cornerCoordinates(pointX, pointY);
-      Scalar circleColor(255.0, 255.0, 255.0, 0.0);
-      int circleThickness=-1; // Filled Circle
-      circle(img_rgb_planes_corners.at(1), cornerCoordinates, radius, circleColor, circleThickness);
+    for (unsigned i=0; i < img_planes_cornerPoints.size(); ++i) {
+      for (unsigned j=0; j < img_planes_cornerPoints.at(i).size(); ++j) {
+	int radius=img.rows/25;
+	int pointX=img_planes_cornerPoints.at(1).at(i).x;
+	int pointY=img_planes_cornerPoints.at(1).at(i).y;
+	Point cornerCoordinates(pointX, pointY);
+	Scalar circleColor(255.0, 255.0, 255.0, 0.0);
+	int circleThickness=-1; // Filled Circle
+	circle(img_planes_corners.at(i), cornerCoordinates, radius, circleColor, circleThickness);
+      }
     }
 
     // Write FPS to Original Image
@@ -146,18 +128,26 @@ int main(int argc, char* argv[]) {
 
     // Display Images
     imshow("Original", img);
-    //imshow("Grayscale", img_gray);
-    //imshow("Blue Channel", img_rgb_planes.at(0));
-    //imshow("Green Channel", img_rgb_planes.at(1));
-    //imshow("Red Channel", img_rgb_planes.at(2));
-    //imshow("Thresholded Grayscale", img_gray_thresh);
-    //imshow("Canny Grayscale", img_gray_canny);
-    //imshow("Corners Grayscale", img_gray_corners);
-    //imshow("Canny Blue Channel", img_rgb_planes_canny.at(0));
-    imshow("Thresholded Green Channel", img_rgb_planes_thresh.at(1));
-    imshow("Canny Green Channel", img_rgb_planes_canny.at(1));
-    imshow("Corners Green Channel", img_rgb_planes_corners.at(1));
-    //imshow("Canny Red Channel", img_rgb_planes_canny.at(2));
+    // Blues
+    //imshow("Blue Channel", img_planes.at(0));
+    //imshow("Thresholded Blue Channel", img_planes_thresh.at(0));
+    //imshow("Canny Blue Channel", img_planes_canny.at(0));
+    //imshow("Corners Blue Channel", img_planes_canny.at(0));
+    // Greens
+    //imshow("Green Channel", img_planes.at(1));
+    imshow("Thresholded Green Channel", img_planes_thresh.at(1));
+    imshow("Canny Green Channel", img_planes_canny.at(1));
+    imshow("Corners Green Channel", img_planes_corners.at(1));
+    // Reds
+    //imshow("Red Channel", img_planes.at(2));
+    //imshow("Thresholded Red Channel", img_planes_thresh.at(2));
+    //imshow("Canny Red Channel", img_planes_canny.at(2));
+    //imshow("Corners Red Channel", img_planes_canny.at(2));
+    // Grayscales
+    //imshow("Grayscale", img_planes.at(3));
+    //imshow("Thresholded Grayscale", img_planes_thresh.at(3));
+    //imshow("Canny Grayscale", img_planes_canny.at(3));
+    //imshow("Corners Grayscale", img_planes_corners.at(3));
 
     if ((waitKey(10) & 255) == 27)
       break;
